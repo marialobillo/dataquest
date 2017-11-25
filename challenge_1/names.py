e1e916b7fd8268152cf4207bf9f44fe8d22d4149
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-11-25 17:53:06 +0100

    the first names

--- challenge_1/names.py
+++ challenge_1/names.py
@@ -1,2 +1,10 @@
-file = open('dq_unisex_names.csv', 'r')
-names = file.read()
+f = open('dq_unisex_names.csv', 'r')
+names = f.read()
+first_five = []
+names_list = names.split('\n')
+for item in range(0,5):
+
+    first_five.append(names_list[item])
+
+
+print(first_five)
